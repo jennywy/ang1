e237443aef44808704b7dc14ec120ebb2138af93
jennywy <jennywy@gmail.com>
2017-12-05 16:19:09 -0500

    Create Server component
    `server.component.ts`
    Create server component typescript file
    Import component from angular core to setup
    Linked component to server component template
    
    `server.component.html`
    Create dummy text in p tag
    
    `index.html`
    played with where and how the app-root loaded

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // configuration as metadata! IMPORTANT
+  selector: 'app-server',
+  templateUrl: './server.component.html'
+})
+
+export class ServerComponent {
+
+}
