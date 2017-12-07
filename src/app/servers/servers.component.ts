a2a48d3c7b417b7505fb9d230ec60ce45ac24214
jennywy <jennywy@gmail.com>
2017-12-07 14:58:47 -0500

    Bind Property `allowNewServer`
    `servers.component.html`
    - create a button with bootstrap attribute `disabled`
    - bind attribute to allowNewServer property
    
    `servers.component.ts`
    - create property `allowNewServer`, a boolean
    - construct a setTimeout() to activate the button after 2 seconds

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,12 +4,17 @@ import { Component, OnInit } from '@angular/core';
   // selector:'app-server', /* to use it as an html tag */
   // selector: '[app-servers]', /* to use it as an attribute */
   selector: '.app-servers', /* to use it as a class */
-  template: '<app-server></app-server><app-server></app-server>',
+  // template: '<app-server></app-server><app-server></app-server>',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  allowNewServer = false;
+  constructor() {
+    setTimeout( () => {
+      this.allowNewServer = true;
+    }, 2000 );
+   }
 
   ngOnInit() {
   }
