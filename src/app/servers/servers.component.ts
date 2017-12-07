407dc05789d822b3e01c47d1310ffd5f67aaf5c7
jennywy <jennywy@gmail.com>
2017-12-07 11:58:32 -0500

    selectors as html tag attribute or class

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  // selector:'app-server', /* to use it as an html tag */
+  // selector: '[app-servers]', /* to use it as an attribute */
+  selector: '.app-servers', /* to use it as a class */
   template: '<app-server></app-server><app-server></app-server>',
   styleUrls: ['./servers.component.css']
 })
