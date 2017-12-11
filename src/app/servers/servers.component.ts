d8f9857a07f07aef1c1f233969b623c6ae38c81d
jennywy <jennywy@gmail.com>
2017-12-11 13:13:00 -0500

    Bind serverName to directive
    `servers.component.html`
    - Add structural directive in p tag that only appears
    after success of adding the server name by binding ngIf to serverCreated property
    
    `servers.component.ts`
    - Create serverCreated property, which is a boolean value to use ngIf
    - Create structural directive, ngIf
    - onCreateServer toggles serverCreated from false to true

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -12,6 +12,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server created';
   serverName = 'Placeholder';
+  serverCreated = false;
 
   constructor() {
     setTimeout( () => {
@@ -23,6 +24,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
     this.serverCreationStatus = "Server was created with name " + this.serverName;
   }
 
