1fa64663a0ce2a06356a151707066869c46c24d7
jennywy <jennywy@gmail.com>
2017-12-18 14:55:49 -0500

    generate components and add navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
-  styles: [`
-  h3 {
-    color: dodgerblue;
-  }
-  `]
+  styleUrls: ['./app.component.css']
+
 })
 export class AppComponent {
-  title = 'Does this change?';
-  name = '';
+
 }
