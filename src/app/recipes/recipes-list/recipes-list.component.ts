f93375f3aa3584d3c2892331c13f5b967eea5048
jennywy <jennywy@gmail.com>
2017-12-20 10:43:56 -0500

    Create Recipe model and use in recipe list

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Title', 'Test Desc', 'https://blog.williams-sonoma.com/wp-content/uploads/2013/03/Grilled-cheese-pull.jpg'),
+    new Recipe('Test Title', 'Test Desc', 'https://blog.williams-sonoma.com/wp-content/uploads/2013/03/Grilled-cheese-pull.jpg')
+  ];
 
   constructor() { }
 
